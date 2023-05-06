b43afae92c1b8fb9afb3269ac550bbacc874c2ae
JinxiangW <103853193+JinxiangW@users.noreply.github.com>
2023-05-06 16:51:42 -0400

    basic data fields and functions for Country and Main

--- src/Country.java
+++ src/Country.java
@@ -1,2 +1,20 @@
+import java.util.Map;
+
 public class Country {
+    private static Map<String, Metrics> countriesData;
+    private String countryName;
+    private Metrics met;
+    public Country(String name, Double[] input) {
+        this.countryName = name;
+        this.met = new Metrics(input);
+        addCountry();
+    }
+
+    private void addCountry() {
+        countriesData.put(countryName, met);
+    }
+    @Override
+    public String toString() {
+        return countryName + ":\n " + met.toString();
+    }
 }
---
+++ src/Metrics.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Metrics {
+    private static final String[] KEYS = new String[]{"humanDevIdx", "genderDevIdx",
+            "genderInequalityIndx", "blowProvertyPercent", "broadband", "happinessScore",
+            "pollutionIdx", "CO2Emission", "lifeExpectancy"};
+    private Map<String, Double> metMap;
+
+    /**
+     * input
+     * @param vals
+     */
+    public Metrics(Double[] vals) {
+        if (KEYS.length != vals.length) {
+            System.err.println("An error occurred. Exiting program.");
+            System.exit(1);
+        }
+
+        metMap = new HashMap<>();
+
+        for (int i = 0; i < KEYS.length; ++i) {
+            metMap.put(KEYS[i], vals[i]);
+        }
+    }
+
+    void setMetric(String key, Double val) {
+        metMap.put(key, val);
+    }
+    Double getMetric(String key) {
+        return metMap.get(key);
+    }
+    @Override
+    public String toString() {
+        String ret = "";
+        for (Map.Entry<String, Double> entry : metMap.entrySet()) {
+            ret += entry.getKey() + ": " + entry.getValue() + "\n";
+        }
+        return ret;
+    }
+}
